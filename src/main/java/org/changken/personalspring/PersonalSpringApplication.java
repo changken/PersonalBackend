efd9b3dd88726b78b054a4da1247b38511f9b525
changken <pp24513@gmail.com>
2023-08-17 15:05:29 +0800

    upgrade spring boot to 3.1.2

--- src/main/java/org/changken/personalspring/PersonalSpringApplication.java
+++ src/main/java/org/changken/personalspring/PersonalSpringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PersonalspringApplication {
+public class PersonalSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PersonalspringApplication.class, args);
+		SpringApplication.run(PersonalSpringApplication.class, args);
 	}
 
 }
